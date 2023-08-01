3fbe7b47f0aca0c536d700de2b31f6bd22407ad9
Anderson Álvarez <al.alvarez439@gmail.com>
2023-08-01 14:54:32 -0500

    [raja]: Some changes

--- src/raja/rajapikernel.cpp
+++ src/raja/rajapikernel.cpp
@@ -5,6 +5,9 @@
 
 #include "RAJA/RAJA.hpp"
 
+
+//CUDA_BLOCK_SIZE specifies the number of threads in a CUDA thread block 
+//256 by default
 #if defined(RAJA_ENABLE_CUDA)
 const int CUDA_BLOCK_SIZE = 256;
 #endif
@@ -13,25 +16,29 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 {
 	std::cout << "Approximate pi using a Riemann sum..." << std::endl;
 
+	//N: number of subintervals (2^30 by default)
 	const int N = 32768 * 32768;
+	//dx: size of each subinterval
 	const double dx = 1.0 / double(N);
 
+	//Set the precision for printing pi
 	int prec = 16;
 
+	//RAJA OpenMP implementation
 	#if defined(RAJA_ENABLE_OPENMP)
 
 		std::cout << "Running RAJA OpenMP pi approximation..." << std::endl;
 
-		using EXEC_POL2 = RAJA::omp_parallel_for_exec;
-		using REDUCE_POL2 = RAJA::omp_reduce;
+		using EXEC_POL1 = RAJA::omp_parallel_for_exec;
+		using REDUCE_POL1 = RAJA::omp_reduce;
 
-		RAJA::ReduceSum <REDUCE_POL2, double> omp_pi(0.0);		
+		RAJA::ReduceSum <REDUCE_POL1, double> omp_pi(0.0);		
 		
 		double omp_totalTime;
 
 		auto omp_t1 = std::chrono::high_resolution_clock::now();
 
-		RAJA::forall <EXEC_POL2>(RAJA::RangeSegment(0, N), [=](int i)
+		RAJA::forall <EXEC_POL1>(RAJA::RangeSegment(0, N), [=](int i)
 		{
 			double x = (double(i) + 0.5) * dx;
 			omp_pi += dx / (1.0 + x * x);
@@ -49,20 +56,21 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 
 	#endif
 
+	//RAJA CUDA implementation
 	#if defined(RAJA_ENABLE_CUDA)
 
 		std::cout << "Running RAJA CUDA pi approximation..." << std::endl;
 
-		using EXEC_POL3 = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
-		using REDUCE_POL3 = RAJA::cuda_reduce;
+		using EXEC_POL2 = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
+		using REDUCE_POL2 = RAJA::cuda_reduce;
 
-		RAJA::ReduceSum <REDUCE_POL3, double> cuda_pi(0.0);
+		RAJA::ReduceSum <REDUCE_POL2, double> cuda_pi(0.0);
 		
 		double cu_totalTime;
 
 		auto cu_t1 = std::chrono::high_resolution_clock::now();
 
-		RAJA::forall <EXEC_POL3>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (int i)
+		RAJA::forall <EXEC_POL2>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (int i)
 		{
 			double x = (double(i) + 0.5) * dx;
 			cuda_pi += dx / (1.0 + x * x);
@@ -75,7 +83,7 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 
 		std::cout << "pi = " << std::setprecision(prec) << cuda_pi_val << std::endl;
 
-                std::cout << "Time elapsep to get the result: " << cu_totalTime << " seconds" << std::endl;
+        std::cout << "Time elapsep to get the result: " << cu_totalTime << " seconds" << std::endl;
 
 	#endif
 
