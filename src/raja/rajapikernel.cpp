ed26ef26b41ee9465217feec3cc1857736147214
Anderson Álvarez <al.alvarez439@gmail.com>
2023-08-01 15:23:19 -0500

    [raja]: Sequential implementation

--- src/raja/rajapikernel.cpp
+++ src/raja/rajapikernel.cpp
@@ -24,21 +24,48 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 	//Set the precision for printing pi
 	int prec = 16;
 
+	std::cout << "Running RAJA sequential pi approximation..." << std::endl;
+
+	using EXEC_POL1 = RAJA:seq_exec;
+	using REDUCE_POL1 = RAJA::seq_reduce;
+
+	RAJA::ReduceSum <REDUCE_POL1, double> seq_pi(0.0);
+
+	double seq_totalTime;
+
+	auto seq_t1 = std::chrono::high_resolution_clock::now();
+
+	RAJA::forall <EXEC_POL1>(RAJA::RangeSegment(0, N), [=](int i)
+	{
+		double x = (double(i) + 0.5) * dx;
+		seq_pi += dx / (1.0 + x * x);
+	});
+
+	double seq_pi_val = seq_pi.get() * 4.0;
+
+	auto seq_t2 = std::chrono::high_resolution_clock::now();
+
+	seq_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(seq_t2 - seq_t1).count();
+
+	std::cout << "pi = " << std::setprecision(prec) << seq_pi_val << std::endl;
+
+	std::cout << "Time elapsep to get the result: " << seq_totalTime << " seconds" << std::endl;
+
 	//RAJA OpenMP implementation
 	#if defined(RAJA_ENABLE_OPENMP)
 
 		std::cout << "Running RAJA OpenMP pi approximation..." << std::endl;
 
-		using EXEC_POL1 = RAJA::omp_parallel_for_exec;
-		using REDUCE_POL1 = RAJA::omp_reduce;
+		using EXEC_POL2 = RAJA::omp_parallel_for_exec;
+		using REDUCE_POL2 = RAJA::omp_reduce;
 
-		RAJA::ReduceSum <REDUCE_POL1, double> omp_pi(0.0);		
+		RAJA::ReduceSum <REDUCE_POL2, double> omp_pi(0.0);		
 		
 		double omp_totalTime;
 
 		auto omp_t1 = std::chrono::high_resolution_clock::now();
 
-		RAJA::forall <EXEC_POL1>(RAJA::RangeSegment(0, N), [=](int i)
+		RAJA::forall <EXEC_POL2>(RAJA::RangeSegment(0, N), [=](int i)
 		{
 			double x = (double(i) + 0.5) * dx;
 			omp_pi += dx / (1.0 + x * x);
@@ -61,16 +88,16 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 
 		std::cout << "Running RAJA CUDA pi approximation..." << std::endl;
 
-		using EXEC_POL2 = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
-		using REDUCE_POL2 = RAJA::cuda_reduce;
+		using EXEC_POL3 = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
+		using REDUCE_POL3 = RAJA::cuda_reduce;
 
-		RAJA::ReduceSum <REDUCE_POL2, double> cuda_pi(0.0);
+		RAJA::ReduceSum <REDUCE_POL3, double> cuda_pi(0.0);
 		
 		double cu_totalTime;
 
 		auto cu_t1 = std::chrono::high_resolution_clock::now();
 
-		RAJA::forall <EXEC_POL2>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (int i)
+		RAJA::forall <EXEC_POL3>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (int i)
 		{
 			double x = (double(i) + 0.5) * dx;
 			cuda_pi += dx / (1.0 + x * x);
