2f5ea33f13057fe1e276482db0c1db557bd2f7bc
Anderson Álvarez <al.alvarez439@gmail.com>
2023-08-11 18:48:57 -0500

    [cuda]: Integration function

--- src/cuda/cudapikernel.cu
+++ src/cuda/cudapikernel.cu
@@ -2,7 +2,7 @@
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
-#define N 32768 * 32768
+#define INTERVALS 32768 * 32768
 
 #define THREADS 256
 #define BLOCKS 64
@@ -20,4 +20,37 @@ inline void checkCUDAError(const char *fileName, const int line)
             exit(-1);
         }
     #endif
+}
+
+__global__ void integration(int *n, float *g_sum)
+{
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    int tx = threadIdx.x;
+
+    __shared__ float s_sum[THREADS];
+
+    double sum = 0.0;
+    double step = 1.0 / (double)*n;
+
+    for (int i = idx + 1; i <= *n; i += blockDim.x * BLOCKS)
+    {
+        double x = step * ((double)i - 0.5);
+        sum += 4.0 / (1.0 + x*x);
+    }
+
+    s_sum[tx] = sum * step;
+
+    __syncthreads();
+
+    for (int i = blockDim.x / 2; i > 0; i >>= 1)
+    {
+        if (tx < i)
+        {
+            s_sum[tx] += s_sum[tx + i];
+        }
+
+        __syncthreads();
+    }
+
+    g_sum[idx] = s_sum[tx];
 }
