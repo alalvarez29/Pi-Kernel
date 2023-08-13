4a2c685c8ffecbab39dfb0a8255f33fa0c02696b
Anderson Álvarez <al.alvarez439@gmail.com>
2023-08-13 17:05:06 -0500

    [cuda]: Optimized kernel

--- src/cuda/cudapikernel.cu
+++ src/cuda/cudapikernel.cu
@@ -6,11 +6,12 @@
 #include <iomanip>
 #include <chrono>
 
-#define INTERVALS 32768 * 32768
+const long STEP_NUM = 32768 * 32768;
+const float STEP_LENGTH = 1.0 / STEP_NUM;
+const int THREAD_NUM = 512;
+const int BLOCK_NUM = 64;
 
-#define THREADS 256
-#define BLOCKS 64
-
+/*
 inline void checkCUDAError(const char *fileName, const int line)
 {
     #ifdef DEBUG
@@ -23,119 +24,42 @@ inline void checkCUDAError(const char *fileName, const int line)
         }
     #endif
 }
+*/
 
-__global__ void integration(int *n, double *g_sum)
+__global__ void integrate(float *globalSum, int stepNum, float, stepLength, int threadNum, int blockNum)
 {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    int tx = threadIdx.x;
+    int globalThreadId = threadIdx.x + blockIdx.x * blockDim.x;
+    int start = (stepNum / (blockNum * threadNum)) * globalThreadId;
+    int end = (stepNum / (blockNum * threadNum)) * (globalThreadId + 1);
+    int localThreadId = threadIdx.x;
+    int blockId = blockIdx.x;
 
-    __shared__ double s_sum[THREADS];
+    __shared__ float blockSum[THREAD_NUM];
 
-    double sum = 0.0;
-    double step = 1.0 / (double)*n;
+    memset(blockSum, 0, threadNum * sizeof(float));
 
-    for (int i = idx + 1; i <= *n; i += blockDim.x * BLOCKS)
+    float x;
+    for(int i = start; i < end; i++)
     {
-        double x = step * ((double)i - 0.5);
-        sum += 4.0 / (1.0 + x*x);
+        x = (i + 0.5f) * stepLength;
+        blockSum[localThreadId] += 1.0f / (1.0f + x * x);
     }
-
-    s_sum[tx] = sum * step;
+    blockSum[localThreadId] *= stepLength * 4;
 
     __syncthreads();
 
-    for (int i = blockDim.x / 2; i > 0; i >>= 1)
+    for(int i = blockDim.x / 2; i > 0; i >>= 1)
     {
-        if (tx < i)
+        if(localThreadId < i)
         {
-            s_sum[tx] += s_sum[tx + i];
+            blockSum[localThreadId] += blockSum[localThreadId + i];
         }
 
         __syncthreads();
     }
 
-    g_sum[idx] = s_sum[tx];
-}
-
-__global__ static void sumReduce(int *n, double *g_sum)
-{
-    int tx = threadIdx.x;
-    __shared__ double s_sum[THREADS];
-
-    if (tx < BLOCKS)
+    if(localThreadId == 0)
     {
-        s_sum[tx] = g_sum[tx * THREADS];
+        globalSum[blockId] = blockSum[0];
     }
-    else
-    {
-        s_sum[tx] = 0.0;
-    }
-
-    for (int i = blockDim.x / 2; i > 0; i >>= 1)
-    {
-        if (tx < i)
-        {
-            s_sum[tx] += s_sum[tx + i];
-        }
-
-        __syncthreads();
-    }
-
-    g_sum[tx] = s_sum[tx];
-}
-
-int main(int argc, char *argv[])
-{
-    int deviceCount = 0;
-
-    std::cout << "Approximate pi using a Riemann sum..." << std::endl;
-    std::cout << std::endl;
-
-    cudaError_t error = cudaGetDeviceCount(&deviceCount);
-
-    if (error != cudaSuccess)
-    {
-        printf("cudaGetDeviceCount returned %d\n-> %s\n", (int)error, cudaGetErrorString(error));
-        return 1;
-    }
-
-    deviceCount == 0 ? printf("There are no available CUDA device(s)\n") : printf("%d CUDA Capable device(s) detected\n", deviceCount);
-
-    int N = INTERVALS; 
-    int *n_d;
-    double pi;
-    double *pi_d;
-
-    cudaMalloc((void**) &n_d, sizeof(int) * 1);
-    cudaMalloc((void**) &pi_d, sizeof(double) * BLOCKS * THREADS);
-
-    cudaMemcpy(n_d, &N, sizeof(int) * 1, cudaMemcpyHostToDevice);
-
-    cudaEvent_t start, stop;
-    float gpuTime;
-
-    cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	
-    cudaEventRecord(start, 0);
-	integration<<<BLOCKS,THREADS>>>(n_d, pi_d);
-	checkCUDAError(__FILE__, __LINE__);
-	sumReduce<<< 1, THREADS >>>(n_d, pi_d);
-	checkCUDAError(__FILE__, __LINE__);
-
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop);
-	cudaEventElapsedTime(&gpuTime, start, stop);
-	
-	cudaMemcpy(&pi, pi_d, sizeof(double) * 1, cudaMemcpyDeviceToHost);
-
-	std::cout << "Pi is: " << pi << std::endl;  
-
-	printf("GPU implementation time: %f ms\n", gpuTime);
-
-	cudaFree(n_d);
-	cudaFree(pi_d);
-
-	cudaDeviceReset();
-	return 0;
 }
