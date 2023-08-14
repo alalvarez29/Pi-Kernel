ef5419448c00b7cfb441493adca662940f4b6cca
Anderson Álvarez <al.alvarez439@gmail.com>
2023-08-13 20:35:20 -0500

    [kokkos]: Some changes

--- src/kokkos/kokkospikernel.cpp
+++ src/kokkos/kokkospikernel.cpp
@@ -1,14 +1,17 @@
-#include <cstdlib>
-#include <iostream>
-#include <iomanip>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
 #include <chrono>
 
 #include <Kokkos_Core.hpp>
 
+const float PI = 3.1415926535897932;
+const long STEP_NUM = 32768 * 32768;
+const float STEP_LENGTH = 1.0 / STEP_NUM;
+
 int main (int argc, char* argv[])
 {
-    std::cout << "Approximate pi using a Riemann sum..." << std::endl;
-	std::cout << std::endl;
+    printf("Approximate pi using a Riemann sum...\n");
 
 	//N: number of subintervals (2^30 by default)
 	const int N = 32768 * 32768;
@@ -34,87 +37,80 @@ int main (int argc, char* argv[])
     using ExecSpace = MemSpace::execution_space;
     using range_policy = Kokkos::RangePolicy<ExecSpace>;
 
-    std::cout << "Running sequential pi approximation..." << std::endl;
+    printf("Running Kokkos sequential pi approximation...\n");
  
-    double seq_pi = 0.0;
+    float seq_pi = 0.0;
 
-    double seq_totalTime;
+    float seq_totalTime;
 
     auto seq_t1 = std::chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < N; ++i) 
+    for (int i = 0; i < STEP_NUM; ++i) 
     {
-        double x = (double(i) + 0.5) * dx;
-        seq_pi += dx / (1.0 + x * x);
+        float x = (float(i) + 0.5) * STEP_LENGTH;
+        seq_pi += STEP_LENGTH / (1.0 + x * x);
     }
 
     seq_pi *= 4.0;
 
     auto seq_t2 = std::chrono::high_resolution_clock::now();
 
-    seq_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(seq_t2 - seq_t1).count();
+    seq_totalTime = std::chrono::duration_cast<std::chrono::duration<float> >(seq_t2 - seq_t1).count();
 
-	std::cout << "\tpi = " << std::setprecision(prec) << seq_pi << std::endl;
-
-	std::cout << "Time elapsed to get the result: " << seq_totalTime << " seconds" << std::endl;
-	std::cout << std::endl;
-
+    printf("PI = %.16lf with error %.16lf\nTime elapsed : %f seconds.\n\n", pi, fabs(seq_pi - PI), seq_totalTime);
+    assert(fabs(pi - PI) <= 0.001);
 
 #if defined(KOKKOS_ENABLE_OPENMP)
 
-	std::cout << "Running Kokkos OpenMP pi approximation..." << std::endl;
+    printf("Running Kokkos OpenMP pi approximation...\n");
 	
-    double omp_pi = 0.0;
+    float omp_pi = 0.0;
 
-    double omp_totalTime;
+    float omp_totalTime;
 
     auto omp_t1 = std::chrono::high_resolution_clock::now();
 
-    Kokkos::parallel_reduce(N, KOKKOS_LAMBDA(const int i, double& omp_pi_val)
+    Kokkos::parallel_reduce(STEP_NUM, KOKKOS_LAMBDA(const int i, float& omp_pi_val)
     {
-        double x = (double(i) + 0.5) * dx;
-		omp_pi_val += dx / (1.0 + x * x);
+        float x = (float(i) + 0.5) * STEP_LENGTH;
+		omp_pi_val += STEP_LENGTH / (1.0 + x * x);
     }, omp_pi);
   
-    double omp_pi_r = omp_pi * 4.0;
+    float omp_pi_r = omp_pi * 4.0;
 
     auto omp_t2 = std::chrono::high_resolution_clock::now();
 
-    omp_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(omp_t2 - omp_t1).count();
+    omp_totalTime = std::chrono::duration_cast<std::chrono::duration<float> >(omp_t2 - omp_t1).count();
 
-	std::cout << "\tpi = " << std::setprecision(prec) << omp_pi_r << std::endl;
-
-	std::cout << "Time elapsed to get the result: " << omp_totalTime << " seconds" << std::endl;
-	std::cout << std::endl;
+	printf("PI = %.16lf with error %.16lf\nTime elapsed : %f seconds.\n\n", pi, fabs(omp_pi_r - PI), omp_totalTime);
+    assert(fabs(omp_pi_r - PI) <= 0.001);
 
 	#endif
 
 #if defined(KOKKOS_ENABLE_CUDA)
 
-    std::cout << "Running Kokkos CUDA pi approximation..." << std::endl;
+    printf("Running Kokkos CUDA pi approximation...\n");
 
-    double cuda_pi = 0.0;
+    float cuda_pi = 0.0;
 
-    double cu_totalTime;
+    float cu_totalTime;
 
     auto cu_t1 = std::chrono::high_resolution_clock::now();
 
-    Kokkos::parallel_reduce(N, KOKKOS_LAMBDA(const int i, double& cu_pi_val)
+    Kokkos::parallel_reduce(STEP_NUM, KOKKOS_LAMBDA(const int i, float& cu_pi_val)
     {
-        double x = (double(i) + 0.5) * dx;
-		cu_pi_val += dx / (1.0 + x * x);
+        float x = (float(i) + 0.5) * STEP_LENGTH;
+		cu_pi_val += STEP_LENGTH / (1.0 + x * x);
     }, cuda_pi);
   
-    double cu_pi_r = cuda_pi * 4.0;
+    float cu_pi_r = cuda_pi * 4.0;
 
     auto cu_t2 = std::chrono::high_resolution_clock::now();
 
-    cu_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(cu_t2 - cu_t1).count();
+    cu_totalTime = std::chrono::duration_cast<std::chrono::duration<float> >(cu_t2 - cu_t1).count();
 
-	std::cout << "\tpi = " << std::setprecision(prec) << cu_pi_r << std::endl;
-
-	std::cout << "Time elapsed to get the result: " << cu_totalTime << " seconds" << std::endl;
-	std::cout << std::endl;
+    printf("PI = %.16lf with error %.16lf\nTime elapsed : %f seconds.\n\n", pi, fabs(cu_pi_r - PI), cu_totalTime);
+    assert(fabs(cu_pi_r - PI) <= 0.001);
 
 #endif
 
