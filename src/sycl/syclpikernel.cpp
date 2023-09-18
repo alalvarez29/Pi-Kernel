66f636ab02949622d4e6bdd1c3be93e6d352fd6b
Anderson Álvarez <al.alvarez439@gmail.com>
2023-09-18 17:15:41 -0500

    Benchmark documentation (15)

--- src/sycl/syclpikernel.cpp
+++ src/sycl/syclpikernel.cpp
@@ -13,12 +13,17 @@ int main(int argc, char* argv[])
     std::cout << "Approximate pi using a Riemann sum..." << std::endl;
     std::cout << std::endl;
 
+    queue q(default_selector_v);
+    std::cout << "Running on: " << q.get_device().get_info<info::device::name>() << std::endl;
+ 
+    //PI constant
     const double PI = 3.1415926535897932;
-    const int N = 32768 * 32768;
-    const double dx = 1.0 / double(N);
-
+	//N: number of subintervals (2^30 by default)
+	const int N = 32768 * 32768;
+	//dx: size of each subinterval
+	const double dx = 1.0 / double(N);
+    //nrepeat: number of repetitions
     int nrepeat = 50;
-    int prec = 16;
 
     double totalTime;
 
@@ -28,43 +33,39 @@ int main(int argc, char* argv[])
     for(int repeat = 0; repeat < nrepeat; repeat++)
     {
         for(int i = 0; i < NTRD; i++) sum[i] = 0.0;
-    {
-        queue q(default_selector_v);
-        range<1> sizeBuf{NTRD};
-        buffer<double, 1> sumBuf(sum.data(), sizeBuf);
-        q.submit([&](handler &h)
         {
-            auto sumAccessor = sumBuf.get_access<access::mode::read_write>(h);
-            h.parallel_for(sizeBuf, [=](id<1> tid)
+            queue q(default_selector_v);
+            range<1> sizeBuf{NTRD};
+            buffer<double, 1> sumBuf(sum.data(), sizeBuf);
+            q.submit([&](handler &h)
             {
-                for(int i=tid; i < N; i+=NTRD)
+                auto sumAccessor = sumBuf.get_access<access::mode::read_write>(h);
+                h.parallel_for(sizeBuf, [=](id<1> tid)
                 {
-                    double x = (double(i) + 0.5) * dx;
-                    sumAccessor[tid] += 4.0 / (1.0 + x * x);
-                }
+                    for(int i=tid; i < N; i+=NTRD)
+                    {
+                        double x = (double(i) + 0.5) * dx;
+                        sumAccessor[tid] += 4.0 / (1.0 + x * x);
+                    }
+                });
             });
-        });
-    }
+        }
 
-    double pi = 0.0;
-    for(int i = 0; i < NTRD; i++)
-    {
-        pi += sum[i];
-    }
-    pi *= dx;
+        double pi = 0.0;
+        for(int i = 0; i < NTRD; i++)
+        {
+            pi += sum[i];
+        }
+        pi *= dx;
 
-    if(repeat == (nrepeat - 1))
-    {
-        queue q(default_selector_v);
-        std::cout << "Running on: " << q.get_device().get_info<info::device::name>() << std::endl;
-        std::cout << "\tpi = " << std::setprecision(prec) << pi << std::endl;
-        std::cout << "\terror = " << std::setprecision(prec) << fabs(pi - PI) << std::endl;
-    }
+        if(repeat == (nrepeat - 1))
+        {
+           std::cout << "\tpi = " << std::setprecision(prec) << pi << std::endl;
+           std::cout << "\terror = " << std::setprecision(prec) << fabs(pi - PI) << std::endl;
+        }
     }
     auto t2 = std::chrono::high_resolution_clock::now();
-    
-    totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
 
-	std::cout << "Time elapsed to get the result: " << totalTime << " seconds" << std::endl;
-	std::cout << std::endl;
+    totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+    std::cout << "Time elapsed to get the result: " << totalTime << " seconds" << std::endl;   
 }
