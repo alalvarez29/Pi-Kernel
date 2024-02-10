c8e0a3ac29d14cacca03609785ed89a09616e7a6
Anderson Álvarez <al.alvarez439@gmail.com>
2024-02-10 14:21:09 -0500

    [cuda]: Added RTX6000 Script

---
+++ scripts/cuda_rtx6000.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Building the Pi Kernel benchmark"
+
+cd ..
+
+cmake -Bbuild -H. -DMODEL=cuda -DCMAKE_CXX_COMPILER=g++ \
+-DCMAKE_CUDA_COMPILER=/usr/local/cuda-12.2/bin/nvcc -DCUDA_ARCH=sm_75
+
+cmake --build build
+
+echo ""
+echo "-------------------------------------"
+echo "Executing the Pi Kernel benchmark"
+echo "Default Parameters"
+echo "Number of steps: 2^36"
+echo "Number of repetitions: 100"
+echo "Precision: 16"
+echo "--------------------------------------"
+echo ""
+
+for i in {0..9..1}
+do
+        ./build/cuda-pikernel
+done
+echo "CUDA Script Pi Kernel executed"
+
+rm -rf build
+
+exit
