bc561fbf58312a1b89083ce8ffa46d2bcf7efbfb
Anderson Álvarez <al.alvarez439@gmail.com>
2024-02-10 17:18:12 -0500

    [kokkos]: Removed Serial implementation

--- src/kokkos/kokkospikernel.cpp
+++ src/kokkos/kokkospikernel.cpp
@@ -38,38 +38,6 @@ int main (int argc, char* argv[])
     using ExecSpace = MemSpace::execution_space;
     using range_policy = Kokkos::RangePolicy<ExecSpace>;
 
-    std::cout << "Running Kokkos sequential pi approximation..." << std::endl;
- 
-    double seq_totalTime;
-
-    auto seq_t1 = std::chrono::high_resolution_clock::now();
-
-    for(int repeat = 0; repeat < nrepeat; repeat++)
-    {
-        double seq_pi = 0.0;
-
-        for (long i = 0; i < N; ++i) 
-        {
-            double x = (i + 0.5) * dx;
-            seq_pi += dx / (1.0 + x * x);
-        }
-
-        seq_pi *= 4.0;
-
-        if(repeat == (nrepeat - 1))
-        {
-            std::cout << "\tpi = " << std::setprecision(prec) << seq_pi << std::endl;
-            std::cout << "\terror = " << std::setprecision(prec) << fabs(seq_pi - PI) << std::endl;    
-        }
-    }
-
-    auto seq_t2 = std::chrono::high_resolution_clock::now();
-
-    seq_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(seq_t2 - seq_t1).count();
-
-	std::cout << "Time elapsed to get the result: " << seq_totalTime << " seconds" << std::endl;
-	std::cout << std::endl;
-    
     //Kokkos OpenMP implementation
     #if defined(KOKKOS_ENABLE_OPENMP)
 
