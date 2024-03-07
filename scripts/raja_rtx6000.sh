d15b2c713a81dcbabd86628de1bc2ed0d5bd5d95
Anderson Álvarez <al.alvarez439@gmail.com>
2024-03-06 20:46:02 -0500

    [raja]: Added RTX6000 Script

---
+++ scripts/raja_rtx6000.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Building the Pi Kernel benchmark"
+
+cd ..
+
+cmake -Bbuild -H. -DMODEL=raja -DCMAKE_CXX_COMPILER=g++ -DRAJA_IN_TREE=/home/cc/RAJA -DENABLE_CUDA=ON -DRAJA_ENABLE_CUDA=ON \
+-DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-12.2 -DCMAKE_CUDA_COMPILER/usr/local/cuda-12.2/bin/nvcc -DCUDA_ARCH=sm_75 -DTARGET=NVIDIA -DRAJA_ENABLE_VECTORIZATION=OFF
+
+cmake --build build
+
+echo ""
+echo "-------------------------------------"
+echo "Executing the Pi Kernel benchmark"
+echo "Default Parameters"
+echo "Number of steps: 2^36"
+echo "Number of repetitions: 100"
+echo "Precision: 16"
+echo "--------------------------------------"
+echo ""
+
+for i in {0..9..1}
+do
+        ./build/raja-pikernel
+done
+echo "RAJA Script Pi Kernel executed"
+
+rm -rf build
+
+exit
--- src/raja/rajapikernel.cpp
+++ src/raja/rajapikernel.cpp
@@ -5,9 +5,6 @@
 
 #include "RAJA/RAJA.hpp"
 
-
-//CUDA_BLOCK_SIZE specifies the number of threads in a CUDA thread block 
-//256 by default
 #if defined(RAJA_ENABLE_CUDA)
 const int CUDA_BLOCK_SIZE = 256;
 #endif
@@ -17,95 +14,87 @@ int main(int RAJA_UNUSED_ARG(argc), char** RAJA_UNUSED_ARG(argv[]))
 	std::cout << "Approximate pi using a Riemann sum..." << std::endl;
 	std::cout << std::endl;
 
-	//PI constant
 	const double PI = 3.1415926535897932;
-	//N: number of subintervals (2^30 by default)
 	const long N = pow(2,36);
-	//dx: size of each subinterval
 	const double dx = 1.0 / double(N);
-	//nrepeat: number of repetitions
-	int nrepeat = 100;
+	const int nrepeat = 100;
+	const int prec = 16;
 
-	//Set the precision for printing pi
-	int prec = 16;
-
-	//RAJA OpenMP implementation
 	#if defined(RAJA_ENABLE_OPENMP)
 
-		std::cout << "Running RAJA OpenMP pi approximation..." << std::endl;
+	std::cout << "Running RAJA OpenMP pi approximation..." << std::endl;
 
-		using EXEC_POL2 = RAJA::omp_parallel_for_exec;
-		using REDUCE_POL2 = RAJA::omp_reduce;
+	using EXEC_POL2 = RAJA::omp_parallel_for_exec;
+	using REDUCE_POL2 = RAJA::omp_reduce;
 
-		double omp_totalTime;
+	double omp_totalTime;
 
-		auto omp_t1 = std::chrono::high_resolution_clock::now();
+	auto omp_t1 = std::chrono::high_resolution_clock::now();
 
-		for(int repeat = 0; repeat < nrepeat; repeat++)
+	for(int repeat = 0; repeat < nrepeat; repeat++)
+	{
+		RAJA::ReduceSum <REDUCE_POL2, double> omp_pi(0.0);
+
+		RAJA::forall <EXEC_POL2>(RAJA::RangeSegment(0, N), [=](long i)
 		{
-			RAJA::ReduceSum <REDUCE_POL2, double> omp_pi(0.0);
+			double x = (double(i) + 0.5) * dx;
+			omp_pi += dx / (1.0 + x * x);
+		});
 
-			RAJA::forall <EXEC_POL2>(RAJA::RangeSegment(0, N), [=](long i)
-			{
-				double x = (double(i) + 0.5) * dx;
-				omp_pi += dx / (1.0 + x * x);
-			});
+		double omp_pi_val = omp_pi.get() * 4.0;
 
-			double omp_pi_val = omp_pi.get() * 4.0;
-
-			if(repeat == (nrepeat -1))
-			{
-				std::cout << "\tpi = " << std::setprecision(prec) << omp_pi_val << std::endl;
-				std::cout << "\terror = " << std::setprecision(prec) << fabs(omp_pi_val - PI) << std::endl;
-			}
+		if(repeat == (nrepeat -1))
+		{
+			std::cout << "\tpi = " << std::setprecision(prec) << omp_pi_val << std::endl;
+			std::cout << "\terror = " << std::setprecision(prec) << fabs(omp_pi_val - PI) << std::endl;
 		}
+	}
 
-		auto omp_t2 = std::chrono::high_resolution_clock::now();
+	auto omp_t2 = std::chrono::high_resolution_clock::now();
 
-		omp_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(omp_t2 - omp_t1).count();
+	omp_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(omp_t2 - omp_t1).count();
 
-		std::cout << "Time elapsed to get the result: " << omp_totalTime << " seconds" << std::endl;
-		std::cout << std::endl;
+	std::cout << "Time elapsed to get the result: " << omp_totalTime << " seconds" << std::endl;
+	std::cout << std::endl;
 
 	#endif
 
-	//RAJA CUDA implementation
 	#if defined(RAJA_ENABLE_CUDA)
 
-		std::cout << "Running RAJA CUDA pi approximation..." << std::endl;
+	std::cout << "Running RAJA CUDA pi approximation..." << std::endl;
 
-		using EXEC_POL3 = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
-		using REDUCE_POL3 = RAJA::cuda_reduce;
+	using EXEC_POL3 = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
+	using REDUCE_POL3 = RAJA::cuda_reduce;
 
 		
-		double cu_totalTime;
+	double cu_totalTime;
 
-		auto cu_t1 = std::chrono::high_resolution_clock::now();
+	auto cu_t1 = std::chrono::high_resolution_clock::now();
 
-		for(int repeat = 0; repeat < nrepeat; repeat++)
+	for(int repeat = 0; repeat < nrepeat; repeat++)
+	{
+		RAJA::ReduceSum <REDUCE_POL3, double> cuda_pi(0.0);
+
+		RAJA::forall <EXEC_POL3>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (long i)
 		{
-			RAJA::ReduceSum <REDUCE_POL3, double> cuda_pi(0.0);
+			double x = (double(i) + 0.5) * dx;
+			cuda_pi += dx / (1.0 + x * x);
+		});
+		double cuda_pi_val = cuda_pi.get() * 4.0;
 
-			RAJA::forall <EXEC_POL3>(RAJA::RangeSegment(0, N), [=] RAJA_DEVICE (long i)
-			{
-				double x = (double(i) + 0.5) * dx;
-				cuda_pi += dx / (1.0 + x * x);
-			});
-			double cuda_pi_val = cuda_pi.get() * 4.0;
-
-			if(repeat == (nrepeat - 1))
-			{
-				std::cout << "\tpi = " << std::setprecision(prec) << cuda_pi_val << std::endl;
-				std::cout << "\terror = " << std::setprecision(prec) << fabs(cuda_pi_val - PI) << std::endl;
-			}
+		if(repeat == (nrepeat - 1))
+		{
+			std::cout << "\tpi = " << std::setprecision(prec) << cuda_pi_val << std::endl;
+			std::cout << "\terror = " << std::setprecision(prec) << fabs(cuda_pi_val - PI) << std::endl;
 		}
+	}
 
-		auto cu_t2 = std::chrono::high_resolution_clock::now();
+	auto cu_t2 = std::chrono::high_resolution_clock::now();
 
-		cu_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(cu_t2 - cu_t1).count();
+	cu_totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(cu_t2 - cu_t1).count();
 
         std::cout << "Time elapsed to get the result: " << cu_totalTime << " seconds" << std::endl;
-		std::cout << std::endl;
+	std::cout << std::endl;
 
 	#endif
 
