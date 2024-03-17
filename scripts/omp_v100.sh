b569350db6a4a0f6a3aca33930ed8cf38196b2b0
Anderson Álvarez <al.alvarez439@gmail.com>
2024-03-17 13:29:37 -0500

    [openmp]: Added V100 Script

---
+++ scripts/omp_v100.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Building the Pi Kernel benchmark"
+
+cd ..
+
+sudo cmake -Bbuild -H. -DMODEL=omp -DCMAKE_CXX_COMPILER=g++ -DOFFLOAD=NVIDIA:sm_70 \
+
+sudo cmake --build build
+
+export OMP_NUM_THREADS=48
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+
+echo ""
+echo "-------------------------------------"
+echo "Executing the Pi Kernel benchmark"
+echo "Default Parameters"
+echo "Number of steps: 2^36"
+echo "Number of repetitions: 100"
+echo "Precision: 16"
+echo "--------------------------------------"
+echo ""
+
+for i in {0..9..1}
+do
+        ./build/omp-pikernel
+done
+echo "OpenMP Script Pi Kernel executed"
+
+sudo rm -rf build
+
+exit
